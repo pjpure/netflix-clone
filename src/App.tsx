1b69a2305fd63b1e8e0055852dfa8f4bb426fc11
pjpure <pure254326@gmail.com>
2022-03-05 04:54:19 +0700

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import NavBar from "./layouts/NavBar/NavBar";
+import Footer from "./layouts/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+
+      <Footer />
     </div>
   );
 }
--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import "./Footer.css";
+import {
+  FaInstagram,
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+} from "react-icons/fa";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer-icon">
+        <FaFacebookF size={25} />
+        <FaInstagram size={25} />
+        <FaTwitter size={25} />
+        <FaLinkedinIn size={25} />
+      </div>
+      <div className="footer-member">
+        <ul>
+          <li>
+            <a href="#">Audio and Subtitles</a>
+          </li>
+          <li>
+            <a href="#">Media Center</a>
+          </li>
+          <li>
+            <a href="#">Privacy</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;

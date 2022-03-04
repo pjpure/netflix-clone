08fb28e39566e7e9c99868b06fc5e872a75f075c
pjpure <pure254326@gmail.com>
2022-03-05 03:58:17 +0700

    add nav bar

---
+++ src/layouts/NavBar/NavBar.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./NavBar.css";
+import netflixLogo from "../../assets/netflix-logo.png";
+import { FaSearch } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+function NavBar() {
+  const [search, setSearch] = useState<string>("");
+  const [isSearch, setIsSearch] = useState<boolean>(false);
+
+  const searchToggle = () => {
+    setIsSearch(!isSearch);
+    setSearch("");
+  };
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <div className="nav-bar">
+      <img src={netflixLogo} alt="Netflix Logo" />
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">TV Shows</a>
+        </li>
+        <li>
+          <a href="#">Movies</a>
+        </li>
+        <li>
+          <a href="#">My List</a>
+        </li>
+      </ul>
+      <div
+        className="nav-search"
+        style={isSearch ? { border: "1px solid #ddd" } : {}}
+      >
+        <FaSearch size={20} onClick={searchToggle} />
+        {isSearch && (
+          <input
+            value={search}
+            onChange={handleSearchChange}
+            type="text"
+            placeholder="Title, people, genres"
+          />
+        )}
+        {search.length > 0 && <ImCross size={20} onClick={searchToggle} />}
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
